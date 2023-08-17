ad9eecc10b513cb731ebf338a5ef9c4b16ca6cb5
Alyssa Hunter <ahunter319@gmail.com>
2023-08-17 12:58:20 -0400

    remove sidbar button

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -33,12 +33,6 @@ function SideBar(props){
             </svg>
         } </button>
 
-        {/* Return to Portfolio */}
-        <a href="https://ahunter319.github.io" data-bs-toggle="tooltip" data-bs-placement="top" title="Home" className="btn btn-outline-info m-2 shadow">
-            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-house" viewBox="0 0 16 16">
-            <path d="M8.707 1.5a1 1 0 0 0-1.414 0L.646 8.146a.5.5 0 0 0 .708.708L2 8.207V13.5A1.5 1.5 0 0 0 3.5 15h9a1.5 1.5 0 0 0 1.5-1.5V8.207l.646.647a.5.5 0 0 0 .708-.708L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.707 1.5ZM13 7.207V13.5a.5.5 0 0 1-.5.5h-9a.5.5 0 0 1-.5-.5V7.207l5-5 5 5Z"/>
-            </svg></a>
-
         {/* Github Link */}
         <a href="https://github.com/ahunter319" data-bs-toggle="tooltip" data-bs-placement="top" title="Code" className="btn btn-outline-info m-2 shadow">
             <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-github" viewBox="0 0 16 16">
