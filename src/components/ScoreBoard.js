af6f29299473e85bd301f84b8f7ecbba36eb3f13
Alyssa Hunter <ahunter319@gmail.com>
2023-08-17 12:51:18 -0400

    fix scoreboard display

--- src/components/ScoreBoard.js
+++ src/components/ScoreBoard.js
@@ -2,9 +2,9 @@ import React from "react";
 
 function ScoreBoard(props){
 
+    let displayTime = props.countDown >= 10 ? "00:" + props.countDown : "00:0" + props.countDown; 
     return <div className="container-fluid justify-content-center align-items-center p-4">
-        <h1>{props.countDown > 10 ? "00:" + props.countDown : "00:0" + props.countDown}</h1> 
-        <h1>{"Score: " + props.score + " wpm"}</h1>
+        <h1>{props.countDown > 0 ? displayTime : "Score: " + props.score + " wpm"}</h1> 
     </div>
     
 }
