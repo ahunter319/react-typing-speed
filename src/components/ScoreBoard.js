f35c40254501f5f777cea1e13d6ba6a897d8b261
Alyssa Hunter <ahunter319@gmail.com>
2023-08-17 12:43:45 -0400

    fix timer display

--- src/components/ScoreBoard.js
+++ src/components/ScoreBoard.js
@@ -3,7 +3,8 @@ import React from "react";
 function ScoreBoard(props){
 
     return <div className="container-fluid justify-content-center align-items-center p-4">
-        <h1>{props.countDown > 0 ? "00:" + props.countDown : "Score: " + props.score + " wpm"}</h1>
+        <h1>{props.countDown > 10 ? "00:" + props.countDown : "00:0" + props.countDown}</h1> 
+        <h1>{"Score: " + props.score + " wpm"}</h1>
     </div>
     
 }
